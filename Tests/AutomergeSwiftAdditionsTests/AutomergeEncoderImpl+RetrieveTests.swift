2e2f558a1f639da7dd1cdb1a32eeecbfe8852622
Joe Heck <heckj@mac.com>
2023-05-17 15:29:41 -0700

    more tests, a few failing with interesting errors

--- Tests/AutomergeSwiftAdditionsTests/AutomergeEncoderImpl+RetrieveTests.swift
+++ Tests/AutomergeSwiftAdditionsTests/AutomergeEncoderImpl+RetrieveTests.swift
@@ -12,27 +12,27 @@ import XCTest
 final class RetrieveObjectIdTests: XCTestCase {
     var doc: Document!
     var setupCache: [String: ObjId] = [:]
-
+    
     override func setUp() {
         setupCache = [:]
         doc = Document()
         let list = try! doc.putObject(obj: ObjId.ROOT, key: "list", ty: .List)
         setupCache["list"] = list
-
+        
         let nestedMap = try! doc.insertObject(obj: list, index: 0, ty: .Map)
         setupCache["nestedMap"] = nestedMap
-
+        
         try! doc.put(obj: nestedMap, key: "image", value: .Bytes(Data()))
         let deeplyNestedText = try! doc.putObject(obj: nestedMap, key: "notes", ty: .Text)
         setupCache["deeplyNestedText"] = deeplyNestedText
     }
-
+    
     func testPathAtRoot() throws {
         let doc = Document()
         let path = try! doc.path(obj: ObjId.ROOT)
         XCTAssertEqual(path, [])
     }
-
+    
     func testRetrieveLeafValue() throws {
         let fullCodingPath: [AnyCodingKey] = [
             AnyCodingKey("list"),
@@ -40,7 +40,7 @@ final class RetrieveObjectIdTests: XCTestCase {
             AnyCodingKey("notes"),
         ]
         let encoderImpl = AutomergeEncoderImpl(userInfo: [:], codingPath: fullCodingPath, doc: doc)
-
+        
         let result = encoderImpl.retrieveObjectId(path: fullCodingPath, containerType: .Value)
         switch result {
         case let .success((objectId, codingKeyInstance)):
@@ -52,14 +52,14 @@ final class RetrieveObjectIdTests: XCTestCase {
         // Caching not yet implemented
         // XCTAssertEqual(encoderImpl.cache.count, 2)
     }
-
+    
     func testCreateSchemaWhereNull() throws {
         let newCodingPath: [AnyCodingKey] = [
             AnyCodingKey("list"),
             AnyCodingKey(1),
         ]
         let encoderImpl = AutomergeEncoderImpl(userInfo: [:], codingPath: newCodingPath, doc: doc)
-
+        
         let result = encoderImpl.retrieveObjectId(path: newCodingPath, containerType: .Key)
         switch result {
         case let .success((objectId, codingKeyInstance)):
@@ -72,4 +72,83 @@ final class RetrieveObjectIdTests: XCTestCase {
         // Caching not yet implemented
         // XCTAssertEqual(encoderImpl.cache.count, 2)
     }
+
+    func testCreateDeeperNewSchema_Key() throws {
+        let newCodingPath: [AnyCodingKey] = [
+            AnyCodingKey("redfish"),
+            AnyCodingKey(0),
+            AnyCodingKey("bluefish"),
+            AnyCodingKey("yellowfish")
+        ]
+        let encoderImpl = AutomergeEncoderImpl(userInfo: [:], codingPath: newCodingPath, doc: doc)
+
+        let result = encoderImpl.retrieveObjectId(path: newCodingPath, containerType: .Key)
+        switch result {
+        case let .success((objectId, codingKeyInstance)):
+            XCTAssertEqual(codingKeyInstance, AnyCodingKey.ROOT)
+            let pathToNewMap = try! doc.path(obj: objectId).stringPath()
+            XCTAssertEqual(pathToNewMap, ".redfish.[0].bluefish.yellowfish")
+        case let .failure(err):
+            XCTFail("Failure looking up new path location: \(err)")
+        }
+    }
+
+    func testCreateDeeperNewSchema_List() throws {
+        let newCodingPath: [AnyCodingKey] = [
+            AnyCodingKey("redfish"),
+            AnyCodingKey(0),
+            AnyCodingKey("bluefish"),
+            AnyCodingKey(0)
+        ]
+        let encoderImpl = AutomergeEncoderImpl(userInfo: [:], codingPath: newCodingPath, doc: doc)
+
+        let result = encoderImpl.retrieveObjectId(path: newCodingPath, containerType: .Index)
+        switch result {
+        case let .success((objectId, codingKeyInstance)):
+            XCTAssertEqual(codingKeyInstance, AnyCodingKey.ROOT)
+            let pathToNewMap = try! doc.path(obj: objectId).stringPath()
+            XCTAssertEqual(pathToNewMap, ".redfish.[0].bluefish.[0]")
+        case let .failure(err):
+            XCTFail("Failure looking up new path location: \(err)")
+        }
+    }
+
+    func testCreateDeeperNewSchema_Value() throws {
+        let newCodingPath: [AnyCodingKey] = [
+            AnyCodingKey("redfish"),
+            AnyCodingKey(0),
+            AnyCodingKey("bluefish"),
+            AnyCodingKey("yellowfish")
+        ]
+        let encoderImpl = AutomergeEncoderImpl(userInfo: [:], codingPath: newCodingPath, doc: doc)
+
+        let result = encoderImpl.retrieveObjectId(path: newCodingPath, containerType: .Value)
+        switch result {
+        case let .success((objectId, codingKeyInstance)):
+            XCTAssertEqual(codingKeyInstance.stringValue, "yellowfish")
+            let pathToNewMap = try! doc.path(obj: objectId).stringPath()
+            XCTAssertEqual(pathToNewMap, ".redfish.[0].bluefish")
+        case let .failure(err):
+            XCTFail("Failure looking up new path location: \(err)")
+        }
+    }
+
+    func testCreateDeeperNewSchema_TooDeepFailure() throws {
+        let newCodingPath: [AnyCodingKey] = [
+            AnyCodingKey("redfish"),
+            AnyCodingKey(5),
+            AnyCodingKey("bluefish"),
+            AnyCodingKey("yellowfish")
+        ]
+        let encoderImpl = AutomergeEncoderImpl(userInfo: [:], codingPath: newCodingPath, doc: doc)
+
+        let result = encoderImpl.retrieveObjectId(path: newCodingPath, containerType: .Value)
+        switch result {
+        case let .success((objectId, codingKeyInstance)):
+            XCTFail("Expected this to fail with index 5 in a new array")
+        case let .failure(err):
+            XCTAssertEqual(err.localizedDescription, "Index value 5 is too far beyond the length: 0 to append a new item.")
+        }
+    }
+
 }
