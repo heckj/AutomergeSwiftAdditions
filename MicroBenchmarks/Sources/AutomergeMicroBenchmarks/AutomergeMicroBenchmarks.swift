bc90f0fcbf9ed45ef2c66e32ee0653c0d09abdd2
Joe Heck <heckj@mac.com>
2023-05-24 16:43:21 -0700

    stubbing in structs for coding sample data and benchmarking

--- MicroBenchmarks/Sources/AutomergeMicroBenchmarks/AutomergeMicroBenchmarks.swift
+++ MicroBenchmarks/Sources/AutomergeMicroBenchmarks/AutomergeMicroBenchmarks.swift
@@ -1,3 +1,44 @@
 import AutomergeSwiftAdditions
+import Foundation
 
 public struct AutomergeMicroBenchmarks {}
+
+public struct GeoLocation: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+    var altitude: Double?
+    var speed: Double?
+    var heading: Double?
+
+    init(latitude: Double, longitude: Double, altitude: Double? = nil, speed: Double? = nil, heading: Double? = nil) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+        self.speed = speed
+        self.heading = heading
+    }
+}
+
+public struct Note: Hashable, Codable {
+    var timestamp: Date
+    var description: String
+    var location: GeoLocation
+    var ratings: [Int]
+
+    init(timestamp: Date, description: String, location: GeoLocation, ratings: [Int]) {
+        self.timestamp = timestamp
+        self.description = description
+        self.location = location
+        self.ratings = ratings
+    }
+}
+
+public struct ExampleModel: Codable {
+    var title: String
+    var notes: [Note]
+
+    init(title: String, notes: [Note]) {
+        self.title = title
+        self.notes = notes
+    }
+}
