932423e74c840111308f098afdc3cb2e9644f2af
Joe Heck <heckj@mac.com>
2023-06-13 14:34:30 -0700

    expanded tests using Counter conforms to Codable

--- Tests/AutomergeSwiftAdditionsTests/AutomergeEncoderDecoderTests.swift
+++ Tests/AutomergeSwiftAdditionsTests/AutomergeEncoderDecoderTests.swift
@@ -11,11 +11,11 @@ import XCTest
 
 final class AutomergeEncoderDecoderTests: XCTestCase {
     var doc: Document!
-
+    
     override func setUp() {
         doc = Document()
     }
-
+    
     func testSimpleEncodeDecode() throws {
         struct SimpleStruct: Codable, Equatable {
             let name: String
@@ -27,12 +27,12 @@ final class AutomergeEncoderDecoderTests: XCTestCase {
             let uuid: UUID
             let notes: Text
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let earlyDate = try Date("1941-04-26T08:17:00Z", strategy: .iso8601)
-
+        
         let sample = SimpleStruct(
             name: "henry",
             duration: 3.14159,
@@ -43,18 +43,50 @@ final class AutomergeEncoderDecoderTests: XCTestCase {
             uuid: UUID(uuidString: "99CEBB16-1062-4F21-8837-CF18EC09DCD7")!,
             notes: Text("Something wicked this way comes.")
         )
-
+        
         try encoder.encode(sample)
         let decodedStruct = try decoder.decode(SimpleStruct.self)
-
+        
         XCTAssertEqual(sample, decodedStruct)
     }
+    
+    func testSimpleCounterEncodeDecode() throws {
+        struct WrapperStruct: Codable, Equatable {
+            let counter: Counter
+        }
 
+        let encoder = AutomergeEncoder(doc: doc)
+        let decoder = AutomergeDecoder(doc: doc)
+
+        let topLevel = WrapperStruct(counter: Counter(5))
+
+        try encoder.encode(topLevel)
+        let decodedStruct = try decoder.decode(WrapperStruct.self)
+        XCTAssertEqual(decodedStruct.counter.value, 5)
+    }
+
+    func testSimpleOptionalCounterEncodeDecode() throws {
+        struct WrapperStruct: Codable, Equatable {
+            let counter: Counter?
+            let anotherOptional: String?
+        }
+
+        let encoder = AutomergeEncoder(doc: doc)
+        let decoder = AutomergeDecoder(doc: doc)
+
+        let topLevel = WrapperStruct(counter: Counter(5), anotherOptional: nil)
+
+        try encoder.encode(topLevel)
+        let decodedStruct = try decoder.decode(WrapperStruct.self)
+        XCTAssertEqual(decodedStruct.counter?.value, 5)
+        XCTAssertNil(decodedStruct.anotherOptional)
+    }
+    
     func testListOfSimpleEncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [SimpleStruct]
         }
-
+        
         struct SimpleStruct: Codable, Equatable {
             let name: String
             let duration: Double
@@ -65,12 +97,12 @@ final class AutomergeEncoderDecoderTests: XCTestCase {
             let uuid: UUID
             let notes: Text
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let earlyDate = try Date("1941-04-26T08:17:00Z", strategy: .iso8601)
-
+        
         let sample = SimpleStruct(
             name: "henry",
             duration: 3.14159,
@@ -82,252 +114,269 @@ final class AutomergeEncoderDecoderTests: XCTestCase {
             notes: Text("Something wicked this way comes.")
         )
         let topLevel = WrapperStruct(list: [sample])
-
+        
         try encoder.encode(topLevel)
-
+        
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(sample, decodedStruct.list.first)
     }
-
+    
     func testListOfFloatEncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Float]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3.0])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3.0, decodedStruct.list.first!, accuracy: 0.1)
     }
-
+    
     func testListOfDoubleEncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Float]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3.0])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3.0, decodedStruct.list.first!, accuracy: 0.1)
     }
-
+    
     func testListOfInt8EncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Int8]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfInt16EncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Int16]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfInt32EncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Int32]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfInt64EncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Int64]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfIntEncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Int]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfUInt8EncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [UInt8]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfUInt16EncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [UInt16]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfUInt32EncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [UInt32]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfUInt64EncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [UInt64]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfUIntEncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [UInt]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [3])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(3, decodedStruct.list.first)
     }
-
+    
     func testListOfDateEncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Date]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let earlyDate = try Date("1941-04-26T08:17:00Z", strategy: .iso8601)
         let topLevel = WrapperStruct(list: [earlyDate])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(earlyDate, decodedStruct.list.first)
     }
-
+    
     func testListOfDataEncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Data]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [Data("Hello".utf8)])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual(Data("Hello".utf8), decodedStruct.list.first)
     }
-
+    
     func testListOfTextEncodeDecode() throws {
         struct WrapperStruct: Codable, Equatable {
             let list: [Text]
         }
-
+        
         let encoder = AutomergeEncoder(doc: doc)
         let decoder = AutomergeDecoder(doc: doc)
-
+        
         let topLevel = WrapperStruct(list: [Text("hi")])
-
+        
         try encoder.encode(topLevel)
         let decodedStruct = try decoder.decode(WrapperStruct.self)
         XCTAssertEqual(decodedStruct.list.count, 1)
         XCTAssertEqual("hi", decodedStruct.list.first?.description)
     }
+
+    func testListOfCounterEncodeDecode() throws {
+        struct WrapperStruct: Codable, Equatable {
+            let list: [Counter]
+        }
+
+        let encoder = AutomergeEncoder(doc: doc)
+        let decoder = AutomergeDecoder(doc: doc)
+
+        let topLevel = WrapperStruct(list: [Counter(3)])
+
+        try encoder.encode(topLevel)
+        let decodedStruct = try decoder.decode(WrapperStruct.self)
+        XCTAssertEqual(decodedStruct.list.count, 1)
+        XCTAssertEqual(3, decodedStruct.list.first?.value)
+    }
+
 }
